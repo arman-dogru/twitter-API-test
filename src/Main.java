d628b5e453c8f965a403e6844a3f21f6ea4a7f6a
Arman Doğru <arman.dogru@gmail.com>
2021-07-05 11:59:12 +0300

    Configurated main for twitter API

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Main {
     public static void main(String[] args) {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("*********************")
+                .setOAuthConsumerSecret("******************************************")
+                .setOAuthAccessToken("**************************************************")
+                .setOAuthAccessTokenSecret("******************************************");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
     }
 }
